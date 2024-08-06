a65661d20cd47677d3e1500e46d722c23ca7df88
Abhirajkar Bajpai <abhirajkar.bajpai@vyld.io>
2024-08-06 17:25:31 +0530

    Redeveloping

--- src/context/ECcontext.jsx
+++ src/context/ECcontext.jsx
@@ -1,13 +1,26 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 const ECContext = createContext();
 
 const ECProvider = ({ children }) => {
 
+  const [sendHint,setSendHint] = useState('');
+  const [sendEmojie,setSendEmojie] = useState('');
+  const [sendAns,setSendAns] = useState('');
+  const [sendNote,setSendNote] = useState('');
+
 
   return (
     <ECContext.Provider
       value={{
+        sendAns,
+        setSendAns,
+        sendEmojie,
+        setSendEmojie,
+        sendHint,
+        setSendHint,
+        sendNote,
+        setSendNote,
       }}
     >
       {children}
